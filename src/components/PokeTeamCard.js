a47a23302ce129a3955ffeafad47ddc4bc94cc8d
Leo Chen <chenl3@rose-hulman.edu>
2021-10-28 11:37:45 -0700

    Nature and Item Functionality

--- src/components/PokeTeamCard.js
+++ src/components/PokeTeamCard.js
@@ -15,7 +15,8 @@ function PokemonCard({pokeCard}) {
         <div className="image" onClick={handleToggleSprite}>
           <img alt="oh no!" src={toggleSprite ? pokeCard.sprites.back : pokeCard.sprites.front}/>
         </div>
-        
+        <div>{pokeCard.nature}</div>
+        <div>{pokeCard.item}</div>
         <br/>
       </div>
     </Card>
--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -9,21 +9,39 @@ function PokemonDetail({selectedPokemon, handleSavedPokemon}){
       setToggleSprite(!toggleSprite)
     }
 
+    const [formData, setFormData] = useState({
+        id: selectedPokemon.id,
+        name: selectedPokemon.name,
+        hp: selectedPokemon.hp,
+        sprites: {
+            front: selectedPokemon.sprites.front,
+            back: selectedPokemon.sprites.back
+        },
+        nature: "",
+        item: ""
+    })
+
+    const handleFormChange = (e) => {
+        // console.log(e.target.name, e.target.value)
+        // console.log(formData.nature, formData.item)
+        setFormData(currentFormData => Object.assign({...currentFormData}, {[e.target.name]:e.target.value}))
+    }
+
     return(
         <>
-                <h1>{selectedPokemon.name}</h1>
-                <div className="image" onClick={handleToggleSprite}>
-                    <img height="250px" alt="oh no!" src={toggleSprite ? selectedPokemon.sprites.back : selectedPokemon.sprites.front}/>
-                </div>
-                <Form className="form">
-                    <Form.Group widths="equal" >
-                        <Form.Input fluid label="Nature" placeholder="Nature" />
-                        <Form.Input fluid label="Item" placeholder="Item" />
-                    </Form.Group>
-                </Form>
-                <Form.Button onClick={() => handleSavedPokemon(selectedPokemon)}>Add to Team</Form.Button>
-                <br/>
-                <br/>
+            <h1>{selectedPokemon.name}</h1>
+            <div className="image" onClick={handleToggleSprite}>
+                <img height="250px" alt="oh no!" src={toggleSprite ? selectedPokemon.sprites.back : selectedPokemon.sprites.front}/>
+            </div>
+            <Form className="form" >
+                <Form.Group widths="equal" >
+                    <Form.Input fluid label="Nature" placeholder="Nature" name="nature" value={formData.nature} onChange={handleFormChange} />
+                    <Form.Input fluid label="Item" placeholder="Item" name="item" value={formData.item} onChange={handleFormChange} />
+                </Form.Group>
+            </Form>
+            <Form.Button onClick={() => {handleSavedPokemon(formData)}}>Add to Team</Form.Button>
+            <br/>
+            <br/>
         </>
         
 
